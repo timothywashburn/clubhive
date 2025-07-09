163cda1d90249935001dd63caee3b1c0c7c4b436
Timothy <timothyrwashburn@gmail.com>
2025-07-08 21:29:26 -0700

    fix startup issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import ApiManager from './controllers/api-manager';
+import ApiManager from '@/controllers/api-manager';
 
 dotenv.config();
 
