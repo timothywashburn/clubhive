607ea8c47647207b12648c33a564bccfba2e3fbb
ominjoo <o.minjoo14@gmail.com>
2025-07-15 23:01:17 -0700

    changed sizing of page

--- client/src/pages/Clubs.tsx
+++ client/src/pages/Clubs.tsx
@@ -22,7 +22,7 @@ export function Clubs() {
 
     return (
         <div className="bg-background">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <div className="mx-auto px-4 sm:px-6 lg:px-8 py-8">
                 <div className="mb-8">
                     <h1 className="text-3xl font-bold text-on-background">
                         Find Clubs
