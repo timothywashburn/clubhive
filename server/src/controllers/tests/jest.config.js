5afe996937ffa2cf1fd1dcc1cd25b02466ac3f43
Keyura Valalla <ke7nv21@gmail.com>
2025-07-22 11:21:24 -0700

    controller test updates2

--- server/src/controllers/tests/jest.config.js
+++
@@ -1,6 +0,0 @@
-export const testEnvironment = 'node';
-export const transform = {
-    '^.+\\.(ts|tsx)$': 'babel-jest', // Use babel-jest for .ts and .tsx files
-};
-export const moduleFileExtensions = ['ts', 'tsx', 'js', 'jsx', 'json', 'node'];
-export const testRegex = '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx|js|jsx)$';
