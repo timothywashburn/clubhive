f4e6a70c637a627acbc7253a855d63c9d756a582
ominjoo <o.minjoo14@gmail.com>
2025-08-06 19:19:11 -0700

    started on cloudinary configuration and new image schema

--- server/src/models/image-schema.ts
+++ server/src/models/image-schema.ts
@@ -6,19 +6,23 @@ const schema = new Schema(
             type: String,
             required: true, // Cloudinary URL
         },
-        _id: {
-            type: mongoose.Schema.Types.ObjectId,
+        public_id: {
+            type: String,
             required: true,
-            auto: true,
         },
         uploadedBy: {
-            type: mongoose.Schema.Types.ObjectId,
+            type: Schema.Types.ObjectId,
             ref: 'User',
             required: true,
         },
         club: {
-            type: mongoose.Schema.Types.ObjectId,
+            type: Schema.Types.ObjectId,
             ref: 'Club',
+            required: true,
+        },
+        type: {
+            type: String,
+            enum: ['profile', 'gallery'],
             required: true,
         },
     },
@@ -28,5 +32,5 @@ const schema = new Schema(
 export type ImageSchema = InferSchemaType<typeof schema>;
 export type ImageDoc = HydratedDocument<ImageSchema>;
 
-const Image = mongoose.model('Image', schema);
-export default Image;
+const CloudinaryImage = mongoose.model('CloudinaryImage', schema);
+export default CloudinaryImage;
--- server/src/services/imageService.ts
+++ server/src/services/imageService.ts
@@ -1,4 +1,14 @@
 import { v2 as cloudinary } from 'cloudinary';
+import { UploadApiResponse } from 'cloudinary';
+
+export const uploadImageToCloudinary = async (filePath: string): Promise<UploadApiResponse> => {
+    try {
+        const res = await cloudinary.uploader.upload(filePath);
+        return res;
+    } catch (err) {
+        throw new Error('Failed to upload image');
+    }
+};
 
 export const deleteImageFromCloudinary = async (public_id: string) => {
     try {
