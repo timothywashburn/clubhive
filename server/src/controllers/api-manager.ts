da29aaed3da2087fec6bd6fff009ce9c70213424
ominjoo <o.minjoo14@gmail.com>
2025-08-12 01:56:19 -0700

    registered and tested endpoints

--- server/src/controllers/api-manager.ts
+++ server/src/controllers/api-manager.ts
@@ -36,6 +36,10 @@ import { getMonthlyVenueAvailabilityEndpoint } from '@/api/venues/monthly-availa
 import { getNotificationsEndpoint } from '@/api/notifications/get-notifications';
 import { markReadEndpoint } from '@/api/notifications/mark-read';
 
+import { uploadImageEndpoint } from '@/api/images/create-image';
+import { deleteImageEndpoint } from '@/api/images/delete-image';
+import { getImageEndpoint } from '@/api/images/get-image';
+
 export default class ApiManager {
     private static instance: ApiManager;
     private readonly router: Router;
@@ -94,6 +98,11 @@ export default class ApiManager {
         this.addEndpoint(getDailyVenueAvailabilityEndpoint);
         this.addEndpoint(getWeeklyVenueAvailabilityEndpoint);
         this.addEndpoint(getMonthlyVenueAvailabilityEndpoint);
+
+        // Images endpoints
+        this.addEndpoint(getImageEndpoint);
+        this.addEndpoint(uploadImageEndpoint);
+        this.addEndpoint(deleteImageEndpoint);
 
         console.log(`registered api endpoints`);
     }
