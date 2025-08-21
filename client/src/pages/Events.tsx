b7058d48fef3262d2b1cb4fda68ae472c98cd230
Timothy <timothyrwashburn@gmail.com>
2025-08-21 13:37:15 -0700

    sort events by upcoming dates

--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -143,6 +143,11 @@ export function Events() {
                   const eventDateTime = new Date(event.date + 'T' + (event.startTime || '00:00'));
                   return eventDateTime.getTime() >= Date.now();
               })
+              .sort((a, b) => {
+                  const dateA = new Date(a.date + 'T' + (a.startTime || '00:00'));
+                  const dateB = new Date(b.date + 'T' + (b.startTime || '00:00'));
+                  return dateA.getTime() - dateB.getTime();
+              })
         : [];
 
     return (
