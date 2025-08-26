8dac2ded0ad94a62d9b77d3c8f7fe4ba6819a559
ominjoo <o.minjoo14@gmail.com>
2025-08-26 16:00:02 -0700

    deleting notifications deletes the pictures from DB and cloud

--- server/src/api/notifications/delete-notification.ts
+++ server/src/api/notifications/delete-notification.ts
@@ -2,8 +2,9 @@ import { ApiEndpoint, AuthType } from '@/types/api-types';
 import { DeleteNotificationResponse } from '@clubhive/shared';
 import Notification from '@/models/notification-schema';
 import UserNotification from '@/models/user-notification-schema';
-// import ImageController from '@/controllers/image-controller';
-// import { deleteImageFromCloudinary } from '@/services/imageService';
+import ImageController from '@/controllers/image-controller';
+import { deleteImageFromCloudinary } from '@/services/imageService';
+import { ImageData } from '@clubhive/shared';
 
 export const deleteNotificationEndpoint: ApiEndpoint<undefined, DeleteNotificationResponse> = {
     path: '/api/notifications/:id',
@@ -12,28 +13,30 @@ export const deleteNotificationEndpoint: ApiEndpoint<undefined, DeleteNotificati
     handler: async (req, res) => {
         try {
             const id = req.params.id;
-            /*
+
             const notification = await Notification.findById(id).populate('pictures');
 
             if (!notification) {
                 res.status(404).json({ success: false, error: { message: 'Notification not found' } });
                 return;
             }
-            
+
             // deleting pictures
             if (notification.pictures && notification.pictures.length > 0) {
                 for (const image of notification.pictures) {
                     try {
-                       // Delete from Cloudinary
-                        await deleteImageFromCloudinary(image.public_id);
+                        const imageData = image as unknown as ImageData;
+
+                        // Delete from Cloudinary
+                        await deleteImageFromCloudinary(imageData.public_id);
 
                         // Delete from database
-                        await ImageController.deleteImage(image._id);
+                        await ImageController.deleteImage(imageData._id);
                     } catch (err) {
-                       
+                        console.error('Error deleting image:', err);
                     }
                 }
-            } */
+            }
 
             const deleted = await Notification.findByIdAndDelete(id);
             if (!deleted) {
